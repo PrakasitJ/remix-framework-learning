665a5454ca514cccb4732c0a53f62a399f439d64
Prakasit J. <big0231@gmail.com>
2024-07-25 00:04:20 +0700

    Change A bit

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base:"/education",
+  base:"/remix-framework-learning",
   plugins: [
     remix({
-      basename:"/education",
+      basename:"/remix-framework-learning",
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
