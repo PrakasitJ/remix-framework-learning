0bfb0b866924e558e503b7d76af0e46343113847
Prakasit J. <big0231@gmail.com>
2024-07-25 00:49:34 +0700

    Change Route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -59,7 +59,7 @@ export default function Index() {
               </div>
             </div>
             <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              <a href="/remix-framework-learning/genshin/character">
+              <a href="/genshin/character">
                 <button
                   type="button"
                   className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto"
@@ -68,7 +68,7 @@ export default function Index() {
                 </button>
               </a>
 
-              <a href="/remix-framework-learning/unpermit">
+              <a href="/unpermit">
                 <button
                   type="button"
                   className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base:"/remix-framework-learning",
   plugins: [
     remix({
-      basename:"/remix-framework-learning",
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
