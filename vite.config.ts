94fdc70aa891d467a1a0f7b8cb58fc455a2f681b
Prakasit J. <big0231@gmail.com>
2024-07-25 00:26:40 +0700

    Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   base:"/remix-framework-learning",
   plugins: [
     remix({
+      ssr: false,
       basename:"/remix-framework-learning",
       future: {
         v3_fetcherPersist: true,
